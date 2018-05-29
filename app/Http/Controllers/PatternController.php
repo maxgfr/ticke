a3797e1a455bb6138be6339ecb3255e286aa1e3e
Maxime Golfier <maxgolfier@gmail.com>
2018-05-29 13:03:11 +0200

    message

--- app/Http/Controllers/PatternController.php
+++ app/Http/Controllers/PatternController.php
@@ -3,8 +3,104 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Pattern;
 
 class PatternController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pattern = Pattern::get();
+        return view('pattern.index', ['pattern' => $pattern]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $pattern = new Pattern();
+        return view('pattern.create', compact('pattern');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\PatternRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PatternRequest $request)
+    {
+        Pattern::create($request->all());
+        return redirect()->route('pattern.index')->with('success', 'Fournisseur ajouté avec succes!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Pattern  $pattern
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pattern $pattern)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $users = User::whereHas('roles', function($q){
+            $q->where('name', 'admin');
+        })->pluck('email', 'id');
+        $pattern = Pattern::findOrFail($id);
+        return view('pattern.edit', compact('pattern', 'users'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\PatternRequest  $request
+     * @param  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(PatternRequest $request, $id)
+    {
+        $pattern = Pattern::findOrFail($id);
+        $pattern->update($request->all());
+        return redirect()->route('pattern.index')->with('success', 'Fournisseur mis à jour!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $pattern = Pattern::findOrFail($id);
+        $pattern->delete();
+        return redirect()->route('pattern.index')->with('error', 'Fournisseur supprimé...');
+    }
 }
